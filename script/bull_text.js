25b888f6c260c53270cffc596154682bd1032b7d
Lucas Madranges <lucas.madranges.pro@icloud.com>
2023-09-21 10:32:33 +0200

    Bouton pour les réponses de l'user. Audio de ketchup et display: none pour les boutons et bulle de texte.

--- script/bull_text.js
+++ script/bull_text.js
@@ -1,7 +1,31 @@
 const bullText = document.getElementById("bull_text");
 const animalCrossingSong = document.getElementById("animal_crossing_song");
 
+const btnDiscussion1 = document.getElementById("btn_discussion_1");
+const btnDiscussion3 = document.getElementById("btn_discussion_3");
+
+const ketchupSong = document.getElementById("ketchup_song");
+
 function HideOffBullText() {
   bullText.style.display = "inline-block";
   animalCrossingSong.play();
+
+  typewriterConfig = [
+    "Bonjour ! <br> Merci d avoir appelé <br> Crazy Fries ! <br> Vous souhaitez prendre un <br> dessert,  une boisson ou un <br> menu complet ? <br> On a de tout ! <br> Et pour pas chère en plus !",
+    true,
+  ];
+  typewriter();
+
+  btnDiscussion1.style.display = "inline-block";
+  btnDiscussion3.style.display = "inline-block";
+}
+
+btnDiscussion3.addEventListener("click", HideOffAll);
+
+function HideOffAll() {
+  btnDiscussion1.style.display = "none";
+  btnDiscussion3.style.display = "none";
+  bullText.style.display = "none";
+
+  ketchupSong.play();
 }
--- script/cursor.js
+++ script/cursor.js
@@ -1,6 +1,6 @@
 const cursor = document.querySelector(".cursor");
 
-cursor.addEventListener("click", (e) => {
+cursor.addEventListener("mousemove", (e) => {
   cursor.setAttribute(
     "style",
     "top:" + (e.pageY - 20) + "px; left:" + (e.pageX - 20) + "px;"
